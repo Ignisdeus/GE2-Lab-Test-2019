b5973efd33f1fe398d38911e310e540c2b3fe292
Michael Carstiars <michael.j.carstairs@gmail.com>
2019-03-26 10:15:29 +0000

    Update Two
    - Fixed bug
    - have drones spawning

--- Assets/Base.cs
+++ Assets/Base.cs
@@ -11,6 +11,7 @@ public class Base : MonoBehaviour
 
     public GameObject fighterPrefab;
     public Color col;
+    public GameObject[] bacesToAttack = new GameObject[3];
 
 
     // Start is called before the first frame update
@@ -25,23 +26,41 @@ public class Base : MonoBehaviour
         //{
         //r.material.color = Color.HSVToRGB(Random.Range(0.0f, 1.0f), 1, 1);
         //}
-        GetComponent<MeshRenderer>().material.color = col; 
+        GetComponent<MeshRenderer>().material.color = col;
+        GameObject[] allBaces = GameObject.FindGameObjectsWithTag("Bace");
+        int heldSpot = 0;
+        for (int i = 0; i < allBaces.Length; i++)
+        {
+            if (allBaces[i].GetComponent<MeshRenderer>().material.color != col)
+            {
+                bacesToAttack[heldSpot] = allBaces[i];
+                heldSpot++;
+            }
+
+        }
     }
 
     // Update is called once per frame
     //spawnCost 
     public int spawnCost = 10;
-    public Transform spawnPoint; 
+    public Transform spawnPoint;
+    public 
+
+    
     void Update()
     {
 
         if(tiberium > spawnCost)
         {
             tiberium -= spawnCost; 
-            Instantiate(fighterPrefab, spawnPoint.position, Quaternion.identity);
-
+            GameObject miniShip = Instantiate(fighterPrefab, spawnPoint.position, Quaternion.identity);
+            miniShip.GetComponentInChildren<MeshRenderer>().material.color = col;
+            fighters.Add(miniShip);
+            
         }
         text.text = "" + tiberium;
+
+        GameObject.FindGameObjectsWithTag("Bace");
     }
 
     public float tiberiumSpawnRate = 1f, amountOfTiberiumPerTick = 1f; 
@@ -49,6 +68,15 @@ public class Base : MonoBehaviour
     {
         // get tiberium at a set rate
         tiberium += amountOfTiberiumPerTick;
+
+    }
+
+    public List<GameObject> fighters; 
+    void FighterBehaviour()
+    {
+
+
+
 
     }
 }
---
+++ Assets/ShootingBehaviour.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootingBehaviour : MonoBehaviour {
+
+    public GameObject[] bacesToAttack = new GameObject [3]; 
+	
+	    
+    void Start()
+    {
+
+        GameObject[] allBaces = GameObject.FindGameObjectsWithTag("Bace");
+        Color col;
+        col = GetComponent<MeshRenderer>().material.color;
+        int heldSpot = 0; 
+        for (int i = 0; i < allBaces.Length; i++)
+        {
+            if(allBaces[i].GetComponent<MeshRenderer>().material.color != col)
+            {
+                bacesToAttack[heldSpot] = allBaces[i];
+                heldSpot++; 
+            }
+
+        }
+
+    }
+	void Update () {
+		
+	}
+}
