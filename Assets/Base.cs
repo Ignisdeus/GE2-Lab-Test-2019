cb3da8fc5c4446134cf9db280397f99c12ca1111
Michael Carstiars <michael.j.carstairs@gmail.com>
2019-03-26 09:38:37 +0000

    Update One
    - Added tiberium Tick
    - added basic camera billboarding

--- Assets/Base.cs
+++ Assets/Base.cs
@@ -10,20 +10,45 @@ public class Base : MonoBehaviour
     public TextMeshPro text;
 
     public GameObject fighterPrefab;
+    public Color col;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        foreach (Renderer r in GetComponentsInChildren<Renderer>())
-        {
-            r.material.color = Color.HSVToRGB(Random.Range(0.0f, 1.0f), 1, 1);
-        }
+       
+        //col = new Color(Random.Range(0, 255), Random.Range(0, 255), Random.Range(0, 255), 255);
+        // start with a bace of 5 tiberiu;
+        tiberium = 5f;
+        InvokeRepeating("GetTiberium", tiberiumSpawnRate, tiberiumSpawnRate);
+        //foreach (Renderer r in GetComponentsInChildren<Renderer>())
+        //{
+        //r.material.color = Color.HSVToRGB(Random.Range(0.0f, 1.0f), 1, 1);
+        //}
+        GetComponent<MeshRenderer>().material.color = col; 
     }
 
     // Update is called once per frame
+    //spawnCost 
+    public int spawnCost = 10;
+    public Transform spawnPoint; 
     void Update()
     {
+
+        if(tiberium > spawnCost)
+        {
+            tiberium -= spawnCost; 
+            Instantiate(fighterPrefab, spawnPoint.position, Quaternion.identity);
+
+        }
         text.text = "" + tiberium;
+    }
+
+    public float tiberiumSpawnRate = 1f, amountOfTiberiumPerTick = 1f; 
+    void GetTiberium()
+    {
+        // get tiberium at a set rate
+        tiberium += amountOfTiberiumPerTick;
+
     }
 }
---
+++ Assets/Look_At_Camera.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Look_At_Camera : MonoBehaviour {
+
+
+
+    public GameObject targetToLookAt; 
+	void Update () {
+        transform.LookAt(targetToLookAt.transform);
+        //transform.rotation = Quaternion.Euler(gameObject.transform.rotation.x , gameObject.transform.rotation.y, gameObject.transform.rotation.z );
+	}
+}
